39c115521c5e57ce914c98f103dcd88318111b84
Paul Sobocinski <paul@humi.ca>
2018-11-17 23:59:05 -0500

    Refactor

--- src/countdown-timer.ts
+++ src/countdown-timer.ts
@@ -1,17 +1,26 @@
-export function CountdownTimer(startTime) {
-  this.startTime = startTime;
-  this.elapsedTime = (readTime) => {
-    let totalElapsedSeconds = (readTime - startTime) / 1000,
-      totalElapsedMinutes = totalElapsedSeconds / 60,
-      totalElapsedHours = totalElapsedMinutes / 60,
-      displayedElapsedHours = Math.floor(totalElapsedHours),
-      displayedElapsedMinutes = Math.floor(totalElapsedMinutes % 60),
-      displayedElapsedSeconds = Math.floor(totalElapsedSeconds % 60);
+export class CountdownTimer {
+  private startTime;
+
+  constructor(startTime) {
+    this.startTime = startTime;
+  }
+
+  public elapsedTime(readTime) {
+    let totalElapsedSeconds = this.totalElapsedSeconds(readTime),
+        totalElapsedMinutes = totalElapsedSeconds / 60,
+        totalElapsedHours = totalElapsedMinutes / 60,
+        displayedElapsedHours = Math.floor(totalElapsedHours),
+        displayedElapsedMinutes = Math.floor(totalElapsedMinutes % 60),
+        displayedElapsedSeconds = Math.floor(totalElapsedSeconds % 60);
 
     return {
       hours: displayedElapsedHours,
       minutes: displayedElapsedMinutes,
       seconds: displayedElapsedSeconds
     }
-  };
-};
+  }
+
+  private totalElapsedSeconds(readTime) {
+    return (readTime - this.startTime) / 1000
+  }
+}
