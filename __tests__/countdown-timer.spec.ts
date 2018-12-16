7202162b9a0ac7965b1a01257ea30b9ee614e186
Paul Sobocinski <psobocinski@gmail.com>
2018-12-15 23:10:15 -0500

    Add stop()

--- __tests__/countdown-timer.spec.ts
+++ __tests__/countdown-timer.spec.ts
@@ -37,7 +37,36 @@ describe('Countdown Timer', () => {
         hours: 1,
         minutes: 1,
         seconds: 1
-      })
-    })
-  })
+      });
+    });
+  });
+
+  describe('stop()', () => {
+    test('resets elapsedTime to zero', () => {
+      let timer = new CountdownTimer(3661, initialTime),
+          timeAfter10secs = new Date(2000, 0, 1, 0, 0, 10);
+
+      timer.stop();
+
+      expect(timer.elapsedTime(timeAfter10secs)).toEqual({
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      });
+    });
+
+    test('resets remaining time to alotted time', () => {
+      let timer = new CountdownTimer(3661, initialTime),
+          timeAfter10secs = new Date(2000, 0, 1, 0, 0, 10);
+
+      timer.stop();
+
+      expect(timer.remainingTime(timeAfter10secs)).toEqual(timer.alottedTime());
+      expect(timer.remainingTime(timeAfter10secs)).toEqual({
+        hours: 1,
+        minutes: 1,
+        seconds: 1
+      });
+    });
+  });
 });
--- src/countdown-timer.ts
+++ src/countdown-timer.ts
@@ -3,19 +3,27 @@ import { Duration } from '../src/duration';
 export class CountdownTimer {
   private alottedSeconds;
   private startTime;
+  private state;
 
   constructor(alottedSeconds, startTime = new Date()) {
     this.alottedSeconds = alottedSeconds;
     this.startTime = startTime;
+    this.state = CountdownTimer.TimerStates.RUNNING;
   }
 
   public elapsedTime(readTime = new Date()) {
+    if (this.state === CountdownTimer.TimerStates.STOPPED)
+      return new Duration(0);
+
     let totalElapsedSeconds = this.totalElapsedSeconds(readTime);
 
     return new Duration(totalElapsedSeconds);
   }
 
   public remainingTime(readTime = new Date()) {
+    if (this.state === CountdownTimer.TimerStates.STOPPED)
+      return this.alottedTime();
+
     let totalRemainingSeconds = this.totalRemainingSeconds(readTime);;
 
     return new Duration(totalRemainingSeconds);
@@ -25,11 +33,20 @@ export class CountdownTimer {
     return new Duration(this.alottedSeconds);
   }
 
+  public stop() {
+    this.state = CountdownTimer.TimerStates.STOPPED;
+  }
+
   private totalRemainingSeconds(readTime) {
     return this.alottedSeconds - this.totalElapsedSeconds(readTime);
   }
 
   private totalElapsedSeconds(readTime) {
     return (readTime - this.startTime) / 1000
+  }
+
+  static TimerStates = class {
+    static RUNNING = Symbol('running');
+    static STOPPED = Symbol('stopped');
   }
 }
